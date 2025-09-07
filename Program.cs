20581a4548b0128b98d6204a4409170a3590f9fd
Temiralp <rtemir1881@gmail.com>
2025-09-07 17:14:20 +0300

    Programın Ana akışı tamamlandı

--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Text.Json;
+
+
+
 public class Program
 {
+    static List<string> gorevler = new List<string>();
 
-static void Main() {
-  
-   
+    static void Main()
+    {
+        Console.WriteLine("gorev ekle 1");
+        Console.WriteLine("gorev listele 2");
+        Console.WriteLine("gorev guncelle 3");
+        Console.WriteLine("gorev sil 4");
+
+        string secim = Console.ReadLine();
+        if(secim == "1")
+        {
+            //
+        }
+
+        if (secim == "2") { 
+        //
+        }
+        if (secim == "3") { 
+        //
+        
+        }
+
+        if (secim == "4") { 
+        //
+        }
+
     }
 
 }
